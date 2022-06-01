6670628a7a67a4c249c12e573e87bf66548c884b
Gagan <gagan123.sp@gmail.com>
2022-06-01 17:10:15 +0530

    hi, stats, kick, ban commands added

--- src/bot.js
+++ src/bot.js
@@ -9,7 +9,7 @@ client.on('ready', ()=>{
     console.log(`${client.user.tag} has logged in`);
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     msg = message.content;
 
@@ -21,23 +21,78 @@ client.on("messageCreate", (message) => {
             message.channel.send(`Heyy! This is Chitti, the Robot. \nI can do whatever you wish me to do so
             \nNice to meet you, ${message.author}`);
         }
-    }
+        else if(CMD_NAME === 'kick'){
+            kick(message, args[0]);
+        }
+        else if(CMD_NAME === 'ban'){
+            ban(message, args[0]);
+        }
+        else if(CMD_NAME ==='stats'){
+            const discordJSVersion = packageJSON.dependencies["discord.js"];
+            const embed = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle(`Bot stats - ${client.user.tag}`)
+                .addField("Discord.js version", discordJSVersion);
+            message.channel.send({
+                embeds: [embed]
+            });
+        }
+        else if(CMD_NAME === 'help'){
 
-    if (message.content == "$stats") {
-        const discordJSVersion = packageJSON.dependencies["discord.js"];
-        const embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
-            .setTitle(`Bot stats - ${client.user.tag}`)
-            .addField("Discord.js version", discordJSVersion);
-        message.channel.send({
-            embeds: [embed]
-        });
+        }
+        else 
+            return message.reply("Couldn't find the command. Try again or run '$help'")
     }
 });
+
 
 client.on('messageDelete',  (message) => {
     message.channel.send(`Haha! Caught you ${message.author} :full_moon_with_face: \nWhy delted the msg: \"${message.content}\"`)
 });
+
+
+function kick(message, arg){
+    if(!message.member.permissions.has('KICK_MEMBERS'))
+        return message.reply("You do not have the permission to use that command");
+
+    if(arg === undefined || arg.length === 0) return message.reply('Please provide an ID of the user');
+
+    if (arg.includes('@')){
+        arg = arg.split('@')[1].split('>')[0];
+    }
+    
+    const member = message.guild.members.cache.get(arg);
+    if(!member) return message.reply("Member not found");
+
+    try { 
+        const user = message.guild.members.kick(arg);
+        message.channel.send(`${user} kicked successfully! `);
+    }
+    catch(err){
+        console.log(err);
+        message.channel.send("I don't have the permission :(");
+    }
+}
+
+function ban(message, arg){
+    if(!message.member.permissions.has('BAN_MEMBERS'))
+        return message.reply("You do not have the permission to use that command");
+    
+    if(arg === undefined || arg.length === 0) return message.reply('Please provide an ID of the user');
+    if (arg.includes('@'))
+        arg = arg.split('@')[1].split('>')[0];
+    
+    const member = message.guild.members.cache.get(arg);
+    if(!member) return message.reply("Member not found");
+    try { 
+        const user = message.guild.members.ban(arg);
+        message.channel.send(`${user} banned successfully! `);
+    }
+    catch(err){
+        console.log(err);
+        message.channel.send("I don't have the permission :(");
+    }
+}
 
 client.login(process.env.TOKEN);
  
